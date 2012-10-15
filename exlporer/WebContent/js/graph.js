2d5d087f17de8ff326c4467d6f09621016d63e6e
olivierlizotte <olivierlizotte@gmail.com>
2012-10-15 14:59:08 -0400

    Augmented browser compatibility (for tablet-like devices)

--- exlporer/WebContent/js/graph.js
+++ exlporer/WebContent/js/graph.js
@@ -1,16 +1,19 @@
-var node,
-    link,
-    root,
-    jsonObj,
-	divName,
-	force,
-	currentWidth,
-	currentHeight,
-	vis,
-	nodes,
-	links,svgLink,
-	fcOnClick;
+var node,//d3 group object for nodes
+    link,//d3 group object for relations
+    root,//root of the graph
+    jsonObj,//data object storing the graph information
+	divName,//identifier for the div containing the graph
+	force,//object defining the behavior of a group of nodes
+	currentWidth,//width of the canvas (divName div)
+	currentHeight,//height of the canvas (divName div)
+	vis,//d3 viewport
+	nodes,//array of the nodes
+	links,//array of relations
+	svgLink,//d3 svg for relations
+	fcOnClick;//Callback function on node click (Mouse up event)
 
+
+//Create the graph
 function CreateGraph(jsonData, divNameParam, OnClick)
 {
 	fcOnClick = OnClick;
@@ -25,80 +28,96 @@ function CreateGraph(jsonData, divNameParam, OnClick)
 		
 	currentWidth = $("#"+divName).width();
     currentHeight = $("#"+divName).height();
-	    
-force = d3.layout.force()
-    .on("tick", tick)
-    .charge(fcCharge)
-    .linkDistance(fcDistance)
-    .size([currentWidth, currentHeight]);
+		    
+	force = d3.layout.force()
+	    .on("tick", tick)
+	    .gravity(0.1)//.05)
+	    .charge(fcCharge)
+	    .linkDistance(fcDistance)
+	    .size([currentWidth, currentHeight]);
+	
+	vis = d3.select("#" + divName).append("svg:svg")
+	    .attr("width", currentWidth)
+	    .attr("height", currentHeight)
+	    .attr("pointer-events", "all");
 
-vis = d3.select("#" + divName).append("svg:svg")
-    .attr("width", currentWidth)
-    .attr("height", currentHeight)
-    .attr("pointer-events", "all");
+	//Create timer for focus animation
+	d3.timer(fcAnimation);
+	
+	jsonObj = jsonData;
+	
+	//Update graph content based on json data object
+	updateGraph();	
 
-//Create timer for focus animation
-d3.timer(fcAnimation);
-  //Zoom behavior breaks the node draging functionnality... so we ditch the zoom for the moment
-  /*.append('svg:g')
-    .call(d3.behavior.zoom().on("zoom", zoomt))
-  .append('svg:g');
-
-    .call(d3.behavior.zoom().on("zoom", zoomt))
-function zoomt() {
-  vis.attr("transform", "translate(" + d3.event.translate + ")scale(" + d3.event.scale + ")");
-}//*/
-
-jsonObj = jsonData;
-updateGraph();
-//root = jsonData;
+	//Register to changes in window size
+	window.onresize = ResizeNavPanel;
 }
 
+//Called every few milliseconds, this function changes the stroke-width of the
+//clicked circle
 function fcAnimation()
 {
     node.selectAll("circle").attr("stroke-width", fcStrokeWidth)
     						.attr("stroke", fcStroke);
 }
 
+//Retrieves the size, in pixel, for the circle of a node
 function fcSize(d) 
 {
-	if(!d || d.size <= 0)
-		return 36;//24;
-	//var size = (d._children ? Math.sqrt(d.cumulSize) : Math.sqrt(d.size)) + 36;//24;
-	var size = (d._children ? Math.sqrt(d.size) : Math.sqrt(d.size)) + 36;//24;
-	if(size > 160)
-		return 160;
+	if(d.pixelSize)
+		return d.pixelSize;
 	else
-		return size;  
+	{
+		var size = Math.sqrt(d.size);
+		
+		if(!d || size <= 36)
+			size = 36;
+		
+		if(size > 160)
+			size = 160;
+		
+		d.pixelSize = size;
+		return size;
+	}
 }
 
+//Retrieves the amount of energy in a node
 function fcCharge(d) 
 {
-	var size = -fcSize(d)*14;
-	if(size > -100)
-		return -100;
+	if(d.pixelCharge)
+		return d.pixelCharge;
 	else
-		return size;  
+	{
+		var charge = -fcSize(d)*50;
+		d.pixelCharge = charge;
+		return charge;
+	}
 }
 
+//Retrieves the expected distance between linked nodes
 function fcDistance(d) 
 {
-	if(!d.source || !d.target)
-		return 100;
-	
-	var size = (fcSize(d.source) + fcSize(d.target)) * 2;	
-	
-	if(size < 40)
-		size = 40;
-	else if(size > 240)
-		size = 240;
-	
-	return size;
+	if(d.pixelDistance)
+		return d.pixelDistance;
+	else
+	{
+		if(!d.source || !d.target)
+			return 100;
+		
+		var distance = (fcSize(d.source) + fcSize(d.target)) * 1.5;	
+		
+		if(distance < 40)
+			distance = 40;
+		
+		d.pixelDistance = distance;
+		return distance;
+	}
 }
 
+//Focus animation activated on node click
 var theFocus;
 var cptStroke = 2.5;
-var incrStroke = 0.12;//0.18
+var incrStroke = 0.12;
 function fcStrokeWidth(d)
 {
 	if(d.focus)
@@ -112,6 +131,7 @@ function fcStrokeWidth(d)
 		return 2.5;
 }
 
+//Retrieves the color of the surrounding line around a circle
 function fcStroke(d)
 {
 	if(d.focus)
@@ -120,42 +140,43 @@ function fcStroke(d)
 		return "#fff";
 }
 
+//Retrieves the text to display in a circle, based on node information
 function fcText(d)
 {
-    //if(d.size && d.size > 1)
-  	//  return d.name + "<br>[" + d.size + "]";
-    //else
   	return d.name;
 }
 
+//Retrieves the angle to rotate text, in order to put tags on relations (links)
 function computeAngle(d)
 {
 	return Math.atan2(d.target.y-d.source.y, d.target.x-d.source.x) * 180/Math.PI;
 }
 
+//Retrieves the length of the line linking two nodes
 function computeLength(d)
 {
 	return Math.sqrt((d.target.x - d.source.x) * (d.target.x - d.source.x) + (d.target.y - d.source.y) * (d.target.y - d.source.y)); 
 }
 
+//Generates the graph based on the info in the jsonObj.
+//This function should get called when the jsonObject content changes.
 function updateGraph() 
 {
-	nodes = jsonObj.nodes;//flatten(root);
-	links = jsonObj.links;//d3.layout.tree().links(nodes);
+	nodes = jsonObj.nodes;
+	links = jsonObj.links;
 
 	root = nodes[0];
 	root.fixed = true;
 	root.x = currentWidth / 2;
 	root.y = currentHeight / 2;
 	
-  // Restart the force layout.
+  // (Re)Start the force layout.
   force
       .nodes(nodes)
       .links(links)
       .start();
 
   // Update the links
-  //link = vis.selectAll("line.link")
   link = vis.selectAll("g.link")
       .data(links, function(d) { return d.target.id; });
 
@@ -163,16 +184,16 @@ function updateGraph()
       .attr("class", "link");
   
   svgLink.append("svg:line")
-      .attr("y1", function(d) { return 8;})
-      .attr("y2", function(d) { return 8;});
+      .attr("y1", "8")
+      .attr("y2", "8");
   
   svgLink.append("svg:line")
-  	  .attr("y1", function(d) { return -8;})
-  	  .attr("y2", function(d) { return -8;});
+  	  .attr("y1", "-8")
+  	  .attr("y2", "-8");
 
   svgLink.append("svg:text")	  
   	  .attr("y", "4")
-	  .text(function(d) {      return ">> " + d.name + " >>";    });
+	  .text(function(d) {      return d.name;    });
   
   // Exit any old links
   link.exit().remove();
@@ -185,24 +206,24 @@ function updateGraph()
   var svgGroup = node.enter().append("svg:g")
       .attr("class", "node")      
       .attr("transform", function(d) { return "translate(" + d.x + "," + d.y + ")"; })
-      .on("mousedown", leftClick)	  
+      .on("mouseup", mouseUp)
+      .on("mousedown", mouseDown)
 	  .on("contextmenu", rightClick)
       .call(force.drag);
   
   //Update state of circles
   vis.selectAll("circle")
   		.transition()
-  			.delay(100)
-  			.attr("r", fcSize)  			
-  			.style("fill", fcColor);
-    
+  			.delay(100);
+  
   svgGroup.append("svg:circle")
       .attr("r", fcSize)
-//      .attr("stroke-width", fcStroke)
-      .style("fill", fcColor);
+      .style("fill-opacity", 0.72)
+      .style("fill", fcColor);  
+
 
   svgGroup.append("svg:text")	  
-  	  .attr("x", function(d) { return -(d.name.length * 0.5 * 5); })
+  	  .attr("x", function(d) { return -(d.name.length * 2.6); })
 	  .text(fcText);
  
   if($('svg g.node').tipsy)
@@ -220,15 +241,20 @@ function updateGraph()
 	        }
 	      });
   }
+   
   // Exit any old nodes.
   node.exit().remove();
 }
 
+//Called periodically, this function updates position of some svg elements to follow the dragged/scattered nodes
 function tick() 
-{
-	link.attr("transform", function(d) { return "translate(" + d.source.x + "," + d.source.y + ") rotate(" + computeAngle(d) + ",0,0)"; });
-	link.selectAll("line").attr("x2", function(d) { return computeLength(d); });
-	link.selectAll("text").attr("x", function(d) { return computeLength(d) * 0.5 - (d.target.name.length * 0.5 * 5); });
+{	
+	link.attr("transform", function(d) { return "translate(" + d.source.x + "," + d.source.y + ") " + 
+			  									"rotate(" + computeAngle(d) + ",0,0) " + 
+			  									"translate(" + fcSize(d.source) + ",0)"; });
+	
+	link.selectAll("line").attr("x2", function(d) { return computeLength(d) - fcSize(d.target) - fcSize(d.source); });
+	link.selectAll("text").attr("x", function(d) { return (computeLength(d) - fcSize(d.target) - fcSize(d.source)) * 0.5 - (d.name.length * 3); });
 
 	node.attr("transform", function(d) { return "translate(" + d.x + "," + d.y + ")"; });	
 }
@@ -237,7 +263,7 @@ function tick()
 function fcColor(d) 
 {
 	if(d.IsRoot)
-		return "#fd8d3c";
+		return "#dd6d2c";
 	if(d.size > 1)
 		return "#70a0cf";
 	if(d._children)
@@ -246,57 +272,38 @@ function fcColor(d)
 }
 
 function rightClick(d) 
-{	/*
-  if (d.children) {
-    d._children = d.children;
-    d.children = null;
-  } else {
-    d.children = d._children;
-    d._children = null;
-  }
-  updateGraph();
-  */
-  //stop showing browser menu
-  d3.event.preventDefault();
+{	
+	d3.event.preventDefault();
 }
 
-function leftClick(d)
+function mouseUp(d)
 {
-	if(!d.IsRoot)
-	{
-		if(theFocus)
-			theFocus.focus = false;
-		d.focus = true;
-		theFocus = d;
-		fcOnClick(d);
-	}
+	d3.event.preventDefault();
 }
 
-// Returns a list of all nodes under the root.
-function flatten(root) {
-  var nodes = [], i = 0;
+function mouseDown(d)
+{
+	for(i = 0; i < nodes.length; i++)	
+		nodes[i].fixed = false;
+	
+	d.fixed = true;	
 
-  function recurse(node) 
-  {
-    if (node.children)
-    	node.cumulSize = node.children.reduce(function(p, v) { return p + recurse(v); }, 0);
-    if (!node.id) node.id = ++i;
-    nodes.push(node);
-    if(node.cumulSize)
-    	return node.cumulSize;
-    else
-    	return node.size;
-  }
-  root.cumulSize = recurse(root);
-  root.IsRoot = true;
-  return nodes;
+	if(theFocus)
+		theFocus.focus = false;
+	
+	d.focus = true;
+	theFocus = d;
+	fcOnClick(d);
+	
+	d3.event.preventDefault();
 }
+
 
 function ResizeNavPanel()
 {
 	if(currentWidth != $("#"+divName).width() || currentHeight != $("#"+divName).height())
 	{		
-		if(root)
+		if(root)//If graph is completly loaded
 		{
 			currentWidth = $("#"+divName).width();
 		    currentHeight = $("#"+divName).height();
@@ -307,12 +314,6 @@ function ResizeNavPanel()
 			   .attr("height", currentHeight);
 			
 			updateGraph();
-		//	$("#" + divName).empty();
-		//	CreateGraph(root, divName);
 		}
 	}	  
 }
-
-//On Window Resize, recompute the graph
-//TODO Call CreateGraph AFTER ExtJS has finished resizing the panel
-//$(window).resize(ResizeNavPanel);
